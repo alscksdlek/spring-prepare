918b79ea36bc964cb66d922e81abd311183fc2da
alscksdlek <mseon916@naver.com>
2025-09-11 16:41:11 +0900

    Lombok과 application.properties2

--- src/main/java/com/sparta/spring_prepare/Memo.java
+++ src/main/java/com/sparta/spring_prepare/Memo.java
@@ -1,4 +1,19 @@
 package com.sparta.spring_prepare;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Memo {
+    private final String username;
+    private String contents;
+}
+
+class Main{
+    public static void main(String[] args) {
+        Memo memo = new Memo("Robbie");
+        System.out.println(memo.getUsername());
+    }
 }
