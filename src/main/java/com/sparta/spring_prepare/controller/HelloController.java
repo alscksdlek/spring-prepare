da0324e837dd51714f464aaf44823151d94550bd
alscksdlek <mseon916@naver.com>
2025-09-11 16:20:11 +0900

    테스트 코드

--- src/main/java/com/sparta/spring_prepare/controller/HelloController.java
+++ src/main/java/com/sparta/spring_prepare/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.sparta.spring_prepare.comtroller;
+package com.sparta.spring_prepare.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
