35775ca5bb4e0216c3416a42ed7c2e084d9185b4
alscksdlek <mseon916@naver.com>
2025-09-13 21:55:14 +0900

    Update, Delete 구현

--- src/main/java/com/sparta/spring_prepare/controller/MemoController.java
+++ src/main/java/com/sparta/spring_prepare/controller/MemoController.java
@@ -37,9 +37,35 @@ public class MemoController {
     @GetMapping("memos")
     public List<MemoResponseDto> getMemos() {
         // Map to List
-        List<MemoResponseDto> responseList = memoList.values().stream()
-                .map(MemoResponseDto::new).toList();
+        List<MemoResponseDto> responseList = memoList.values().stream().map(MemoResponseDto::new).toList();
 
         return responseList;
+    }
+
+    @PutMapping("/memos/{id}")
+    public long updateMemo(@PathVariable long id, @RequestBody MemoRequestDto requestDto) {
+        // 해당 메모가 DB에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            // 해당 메모 가져오기
+            Memo memo = memoList.get(id);
+
+            // memo 수정
+            memo.update(requestDto);
+            return memo.getId();
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+    @DeleteMapping("memos/{id}")
+    public long deleteMemo(@PathVariable long id) {
+        // 해당 메모가 DB에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            // 해당 메모 삭제하기
+            memoList.remove(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
     }
 }
